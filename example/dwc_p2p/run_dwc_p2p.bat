8882d60469317fd9216d6ed970f71bf0af248781
CosmicScholar <zhikuiguo@live.cn>
2019-06-20 15:45:46 +0200

    add win64

---
+++ example/dwc_p2p/run_dwc_p2p.bat
@@ -0,0 +1,7 @@
+set field_path=..\data
+set field_in=mag_plane_8
+set field_out=%field_in%_dwc_p2p.grd
+set height_dwc=8
+..\..\bin\conti2d %field_path%/%field_in%.grd -G%field_out% -E5 -H0 -T%height_dwc% -D+L10  
+
+pause
--- windows/example/uwc_p2p/run_uwc_p2p.bat
+++
@@ -1,8 +0,0 @@
-set field_path=../data
-set field_in=mag_plane_0
-set field_out=%field_in%_uwc_p2p.vtk
-set height_uwc=8
-D:\SoftwareProject\conti2d\windows\conti2d\Release\conti2d %field_path%/%field_in%.grd -G%field_out% -H%height_uwc%
-
-
-pause
