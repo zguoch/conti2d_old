f0d17ba5b4053fe2a38e553f7877ff3e1277200b
CosmicScholar <zhikuiguo@live.cn>
2019-06-20 15:33:27 +0200

    add release 2.0 win32

---
+++ example/uwc_p2p/run_uwc_p2p.bat
@@ -0,0 +1,8 @@
+set field_path=..\data
+set field_in=mag_plane_0
+set field_out=%field_in%_uwc_p2p.vtk
+set height_uwc=8
+..\..\bin\conti2d %field_path%\%field_in%.grd -G%field_out% -H%height_uwc%
+
+
+pause
---
+++ example/uwc_p2p_f/run_uwc_p2p_f.bat
@@ -0,0 +1,6 @@
+set field_path=..\data
+set field_in=mag_plane_0
+set field_out=%field_in%_uwc_p2p_f.nc
+..\..\bin\conti2d %field_path%\%field_in%.grd -G%field_out% -H8 -f 
+
+pause
